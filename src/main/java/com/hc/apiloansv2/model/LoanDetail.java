16229d767802a8fc978eb06cf0d4d314b43275e1
Mario Alejandro Huaypuna <mhuaypuna@unsa.edu.pe>
2019-08-31 15:57:52 -0500

    Updating LoanDetail

--- src/main/java/com/hc/apiloansv2/model/LoanDetail.java
+++ src/main/java/com/hc/apiloansv2/model/LoanDetail.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * esTA es mi prueba de modificacion en el master
  */
 package com.hc.apiloansv2.model;
 
